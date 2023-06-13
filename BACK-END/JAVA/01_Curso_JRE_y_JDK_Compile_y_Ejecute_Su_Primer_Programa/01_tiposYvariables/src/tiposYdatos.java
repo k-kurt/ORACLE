81ad37c78b8057950c580e8c98dff62c644b9a32
k-kurt <sboboda1997@gmail.com>
2023-06-12 21:09:11 -0300

    tipo y variables

---
+++ BACK-END/JAVA/01_Curso_JRE_y_JDK_Compile_y_Ejecute_Su_Primer_Programa/01_tiposYvariables/src/tiposYdatos.java
@@ -0,0 +1,17 @@
+
+public class tiposYdatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		
+		int numero=15;
+		System.out.println("el numero es: "+numero );
+		numero=25;
+		System.out.println("el numero ahora es: "+numero);
+		numero=25;
+		System.out.println("el numero ahora es: "+(numero+20));
+		
+	}
+
+}
