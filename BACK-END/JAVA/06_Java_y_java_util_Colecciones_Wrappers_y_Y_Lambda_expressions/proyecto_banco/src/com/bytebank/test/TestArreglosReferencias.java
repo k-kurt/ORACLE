231c7d7a3b8551421bda0e03d80eafbcb84e90d8
k-kurt <sboboda1997@gmail.com>
2023-09-06 22:59:54 -0300

    Terminado Cast Object #2

--- BACK-END/JAVA/06_Java_y_java_util_Colecciones_Wrappers_y_Y_Lambda_expressions/proyecto_banco/src/com/bytebank/test/TestArreglosReferencias.java
+++ BACK-END/JAVA/06_Java_y_java_util_Colecciones_Wrappers_y_Y_Lambda_expressions/proyecto_banco/src/com/bytebank/test/TestArreglosReferencias.java
@@ -1,5 +1,7 @@
 package com.bytebank.test;
 
+import com.bytebank.modelo.Cliente;
+import com.bytebank.modelo.CuentaAhorros;
 import com.bytebank.modelo.CuentaCorriente;
 
 public class TestArreglosReferencias {
@@ -10,19 +12,40 @@ public class TestArreglosReferencias {
 		
 		CuentaCorriente	corriente=new CuentaCorriente(23, 44);
 		
+		CuentaAhorros ca=new CuentaAhorros(2, 22);
 		
 		
-		CuentaCorriente[] cuentas=new CuentaCorriente[5];
+		Object[] cuentas=new Object[5];
 		
 		cuentas[1]=corriente;
-		
-		System.out.println(corriente);
-		
-		System.out.println(cuentas[1]);
+		cuentas[2]=ca;
+
 		
 		cuentas[0]=new CuentaCorriente(32, 12);
 		
-		System.out.println(cuentas[0]);
+		Cliente cliente=new Cliente();
+		cuentas[3]=cliente;
+		
+		//obtener un cliente del array
+		Cliente obtenido= (Cliente)cuentas[3];
+		System.out.println(obtenido);
+		
+		
+
+		//obtener una cuenta del array
+		
+		//CuentaCorriente cuenta=cuentas[1]; //no compila por que cuentas[1] hace referencia
+//		al array de tipo cuentas, que esta mas arriba y es mas absorbente, no es de tipo cuentaCorriente
+//			
+//		LA SOLUCION ES HACER CASTING
+		
+		CuentaCorriente cuenta= (CuentaCorriente)cuentas[1]; 
+		//estoy diciendole a java que este objecto cuenta es una cuenta corriente entonces ahora si
+		//compila
+		System.out.println(cuenta);
+		
+		
+		
 		
 		for(int i=0;i<cuentas.length;i++) {
 			System.out.println(cuentas[i]);
