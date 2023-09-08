c5fad5a12d2bb8ae0edaa4b3caf533ef2272639e
k-kurt <sboboda1997@gmail.com>
2023-09-07 22:55:12 -0300

    guardar cuentas

--- BACK-END/JAVA/06_Java_y_java_util_Colecciones_Wrappers_y_Y_Lambda_expressions/proyecto_banco/src/com/bytebank/modelo/GuardaCuentas.java
+++ BACK-END/JAVA/06_Java_y_java_util_Colecciones_Wrappers_y_Y_Lambda_expressions/proyecto_banco/src/com/bytebank/modelo/GuardaCuentas.java
@@ -5,6 +5,26 @@ public class GuardaCuentas {
 	
 	//Crear un objeto para guardar muchas cuentas
 	//permitirnos agregar cuentas con un metodo
+	//guardarCeuntas.adiciona(cuenta);
+	//obtener.remover,etc.
 	
+	Cuenta[] cuenta=new Cuenta[10];
+
+	int indice=0;
+	
+	public void adicionar(Cuenta cc) {
+		// TODO Auto-generated method stub
+		
+		cuenta[indice]=cc;
+		indice++;
+		
+	}
+
+	public void obtener(int indice) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(cuenta[indice]);
+		
+	}
 
 }
---
+++ BACK-END/JAVA/06_Java_y_java_util_Colecciones_Wrappers_y_Y_Lambda_expressions/proyecto_banco/src/com/bytebank/test/TestGuardaCuenta.java
@@ -0,0 +1,25 @@
+package com.bytebank.test;
+
+import com.bytebank.modelo.Cuenta;
+import com.bytebank.modelo.CuentaAhorros;
+import com.bytebank.modelo.CuentaCorriente;
+import com.bytebank.modelo.GuardaCuentas;
+
+public class TestGuardaCuenta {
+
+	public static void main(String[] args) {
+		GuardaCuentas guardarCuenta=new GuardaCuentas();
+		Cuenta cc=new CuentaCorriente(1, 2);
+		Cuenta ca=new CuentaAhorros(10, 20);
+		guardarCuenta.adicionar(cc);
+		guardarCuenta.adicionar(ca);
+		
+		guardarCuenta.obtener(0);
+
+		guardarCuenta.obtener(1);
+		
+		
+		
+		
+	}
+}
