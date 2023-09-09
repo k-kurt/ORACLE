0b93ae0d954f5ee17b9185152f63bad61e0799a2
k-kurt <sboboda1997@gmail.com>
2023-09-09 02:38:09 -0300

    Terminado Comparando Elementos

--- BACK-END/JAVA/06_Java_y_java_util_Colecciones_Wrappers_y_Y_Lambda_expressions/proyecto_banco/src/com/bytebank/test/TestArrayList.java
+++ BACK-END/JAVA/06_Java_y_java_util_Colecciones_Wrappers_y_Y_Lambda_expressions/proyecto_banco/src/com/bytebank/test/TestArrayList.java
@@ -10,7 +10,7 @@ public class TestArrayList {
 	
 	public static void main(String[] args) {
 		
-		ArrayList lista=new ArrayList();
+		ArrayList<Cuenta> lista=new ArrayList<Cuenta>();
 		
 		Cuenta cc=new CuentaCorriente(10, 20);
 		Cuenta c2=new CuentaCorriente(20, 30);
@@ -20,7 +20,26 @@ public class TestArrayList {
 		lista.add(c2);
 		
 		Cuenta obtenerCuenta=(Cuenta) lista.get(0);
-		System.out.println(obtenerCuenta);
+		
+		for(int i=0;i<lista.size();i++) {
+			
+		System.out.println(lista.get(i));
+		}
+		
+		for (Cuenta cuenta : lista) {
+			System.out.println(cuenta);
+			//imprime por cada cuenta : en la lista
+		}
+		
+		
+		boolean contiene=lista.contains(c2);
+		if(contiene) {
+			System.out.println("Si");
+		}
+		
+		
+		
+		
 	}
 	
 }
