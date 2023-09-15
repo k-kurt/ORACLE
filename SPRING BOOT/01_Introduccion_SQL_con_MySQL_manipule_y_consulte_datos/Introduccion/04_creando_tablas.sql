a9d9a9fdd87ebf1f3d09ac4d755a0111396e228c
k-kurt <sboboda1997@gmail.com>
2023-09-15 11:57:21 -0300

    quede en insertando registros en la tabla

---
+++ SPRING BOOT/01_Introduccion_SQL_con_MySQL_manipule_y_consulte_datos/Introduccion/04_creando_tablas.sql
@@ -0,0 +1,9 @@
+use world;
+use jugos;
+create table tbclientes3(dni varchar(20), nombre varchar(150), direccion1 varchar(150), direccion2 varchar(150), barrio varchar(50), ciudad varchar(50),
+estado varchar(50), cp varchar(10), edad smallint, sexo varchar(1), limite_credito float, volumen_compra float, primera_compra bit(1));
+CREATE TABLE TABLA_DE_VENDEDORES (
+    MATRICULA varchar(5), 
+    NOMBRE varchar(100), 
+    PORCENTAJE_COMISION float);
+    
