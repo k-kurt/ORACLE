2cbbbde51d8c30b10b66005aa01f2dd6fb3deefb
k-kurt <sboboda1997@gmail.com>
2023-09-21 20:07:51 -0300

    termine creando pool

---
+++ SPRING BOOT/02_Java_y_JDBC_trabajando_con_una_base_de_datos/04_escabilidad_pool_conexiones/Escabilidad/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -0,0 +1,40 @@
+package com.alura.jdbc.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+	
+	
+	private DataSource datasource;
+	
+	
+	//vamos a crear una variable que instancie al c3p0
+	public ConnectionFactory() {
+		var pooledDataSource=new  ComboPooledDataSource();
+		//pasamos los parametros de la conexion
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("kevin");
+		
+		this.datasource=pooledDataSource;
+	}
+	
+	public Connection recuperaConexion() throws SQLException {
+	
+		 return this.datasource.getConnection();
+	}
+
+}
+
+
+
+
+
+
+
